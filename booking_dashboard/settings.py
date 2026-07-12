12f16f9400cce0527028dde657f09fc5663b82e3
abhishek-kumar <shadowatg00@gmail.com>
2026-07-12 21:10:22 +0530

    added landing page to the platform

--- booking_dashboard/settings.py
+++ booking_dashboard/settings.py
@@ -91,3 +91,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
--- properties/urls.py
+++ properties/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Auth
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
+
     # Pages
     path('', views.index, name='index'),
     path('calendar/', views.calendar_view, name='calendar'),
